2dac9f4d4cdae895a357a49653905d230a6f69bd
Samuel <1224042766@qq.com>
2019-12-09 12:13:53 +0800

    Update ask.js

--- ask.js
+++ ask.js
@@ -4,5 +4,24 @@ module.exports = [
     type: 'confirm',
     name: 'private',
     message: 'this resgistery is private?',
+    default: true
+  },
+  {
+    type: 'confirm',
+    name: 'author',
+    message: 'use default author "SF"?',
+    default: true
+  },
+  {
+    type: 'confirm',
+    name: 'description',
+    message: 'use default author "description"?',
+    default: true
+  },
+  {
+    type: 'confirm',
+    name: 'license',
+    message: 'use license?',
+    default: false
   }
 ]
